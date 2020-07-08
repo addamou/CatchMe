b4de31b72d903cf15331b7b260c30fd4b600b703
Addamou <adamou.halidou@codeloccol.org>
2020-07-08 13:18:14 +0100

    catch me

---
+++ img/app.js
@@ -0,0 +1,49 @@
+  //====================variables====================================
+  let trous = document.querySelectorAll('.trou');
+  let scoreBoard = document.querySelector('.score');
+  let animals = document.querySelectorAll('.animal');
+  let lasttrou;
+  let timeUp = false;
+  let score = 0;
+ //====================== fonction aleatoire =========================AHS=======
+  function randomTime(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+  }
+//=========================================trou=================================
+  function randomtrou(trous) {
+    let idx = Math.floor(Math.random() * trous.length);
+    let trou = trous[idx];
+    if (trou === lasttrou) {
+      //console.log('Ah nah thats the same one bud');
+      return randomtrou(trous);
+    }
+    lasttrou = trou;
+    return trou;
+  }
+//mouvement du renard et sa vitesse
+  function peep() {
+    let time = randomTime(400, 1000);
+    let trou = randomtrou(trous);
+    trou.classList.add('up');
+    setTimeout(() => {
+      trou.classList.remove('up');
+      if (!timeUp) peep();
+    }, time);
+  }
+//================================================================
+  function startGame() {
+    scoreBoard.textContent = 0;
+    timeUp = false;
+    score = 0;
+    peep();
+    setTimeout(() => timeUp = true, 10000)
+  }
+
+  function bonk(e) {
+    if(!e.isTrusted) return; // cheater!
+    score++;
+    this.parentNode.classList.remove('up');
+    scoreBoard.textContent = score;
+  }
+
+  animals.forEach(animal => animal.addEventListener('click', bonk));
